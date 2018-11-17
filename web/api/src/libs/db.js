15c451c1a7b6e9a36b50b244e15bf48bb4a63889
Jirachai C <antronic.inc@gmail.com>
2018-11-17 23:07:19 +0700

    Add attemping to reconnect database

--- web/api/src/libs/db.js
+++ web/api/src/libs/db.js
@@ -4,11 +4,37 @@ import mongoose from 'mongoose'
 
 const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME
 
-export default mongoose.connect(url, {
-  useNewUrlParser: true,
-})
-  .then(() => console.log('Database already started!'))
-  .catch((err) => {
-    console.log('Database error!')
-    console.error(err)
+let connected = false
+let connecting = true
+
+const connect = () => {
+  const intv = setInterval(() => {
+    if (connected) {
+      clearInterval(intv)
+    }
+
+    if (connecting) {
+      console.err('Attempting to reconnect...')
+      connect()
+    }
+  }, 1000)
+
+  connecting = true
+
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
   })
+    .then(() => {
+      connected = true
+      connecting = false
+      console.log('Database already started!')
+    })
+    .catch((err) => {
+      connecting = false
+      console.log('Database error!')
+      console.error(err)
+    })
+
+}
+
+export default connect()
