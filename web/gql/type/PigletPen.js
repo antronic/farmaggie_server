f6ecc1f83c3878bc1689646e4fc0e31d865a3424
bazsup <supawit.ruen@mail.kmutt.ac.th>
2019-11-06 12:53:00 +0700

    Update: graphql type to query _id

--- web/gql/type/PigletPen.js
+++ web/gql/type/PigletPen.js
@@ -1,17 +1,20 @@
 module.exports = `
+  type PigletPenVaccineInjection {
+    _id: String
+    fmd: String
+    csf: String
+    pseudo: String
+    created_at: String
+    updated_at: String
+  }
+
   type PigletPen {
     _id: String
     pig_amount: Int
-    vaccine_injection: [BreederVaccineInjection]
+    vaccine_injection: [PigletPenVaccineInjection]
     coop_number: Int
     created_at: String
     updated_at: String
   }
 
-  type PigletPenVaccineInjection {
-    _id: String!
-    fmd: String
-    csf: String
-    pseudo: String
-  }
 `
